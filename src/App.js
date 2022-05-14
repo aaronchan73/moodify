ec3aeeec61d92ab8b1b494c59e04747ab665e943
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-05-14 03:30:45 -0700

    Added .env and fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import SongsList from './components/SongsList';
-import Vision from './components/Vision';
+
 
 function App() {
   return (
     <div className="App">
       <SongsList />
-      {/* <Vision /> */}
     </div>
   );
 }
 
-export default mainPage;
+export default App;
