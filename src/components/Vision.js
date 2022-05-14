91187bc73ab421940de1e8320bad2c6bd7999ec8
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-05-14 02:50:49 -0700

    commented out vision for merge

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -1,60 +1,57 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 
-const Vision = () => {
+// const Vision = () => {
 
-    const [dict, setDict] = useState({});
-    const [answer, setAnswer] = useState("");
+//     const [dict, setDict] = useState({});
+//     const [answer, setAnswer] = useState("");
+//     const fileName = "./image.jpg"
 
-    const vision = require('@google-cloud/vision');
-    const api_key = "AIzaSyBB-xv8yyuki1oQs54SQ-nfVcHq90yOtD0";
+//     const vision = require('@google-cloud/vision');
 
-    const client = new vision.ImageAnnotatorClient();
+//     const client = new vision.ImageAnnotatorClient();
 
-    useEffect(() => {
-        detectFaces();
-        rankEmotions();
-    }, []);
+//     useEffect(() => {
+//         detectFaces();
+//         console.log(dict)
+//         // rankEmotions();
+//     }, []);
 
-    async function detectFaces() {
-        /**
-         * TODO(developer): Uncomment the following line before running the sample.
-         */
-        // const fileName = 'Local image file, e.g. /path/to/image.png';
+//     async function detectFaces() {
 
-        const [result] = await client.faceDetection(fileName);
-        const faces = result.faceAnnotations;
-        console.log('Faces:');
-        faces.forEach((face, i) => {
-            console.log(`  Face #${i + 1}:`);
-            console.log(`    Joy: ${face.joyLikelihood}`);
-            console.log(`    Anger: ${face.angerLikelihood}`);
-            console.log(`    Sorrow: ${face.sorrowLikelihood}`);
-            console.log(`    Surprise: ${face.surpriseLikelihood}`);
-            setDict({
-                joy: `${face.joyLikelihood}`,
-                anger: `${face.angerLikelihood}`,
-                sorrow: `${face.sorrowLikelihood}`,
-                surprise: `${face.surpriseLikelihood}`
-            });
-        });
-    }
+//         const result = await client.faceDetection(fileName);
+//         const faces = await result.faceAnnotations;
+//         console.log('Faces:');
+//         faces.forEach((face, i) => {
+//             console.log(`  Face #${i + 1}:`);
+//             console.log(`    Joy: ${face.joyLikelihood}`);
+//             console.log(`    Anger: ${face.angerLikelihood}`);
+//             console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+//             console.log(`    Surprise: ${face.surpriseLikelihood}`);
+//             setDict({
+//                 joy: `${face.joyLikelihood}`,
+//                 anger: `${face.angerLikelihood}`,
+//                 sorrow: `${face.sorrowLikelihood}`,
+//                 surprise: `${face.surpriseLikelihood}`
+//             });
+//         });
+//     }
 
-    function rankEmotions() {
-        for (var key in dict) {
-            if (dict[key] == "VERY_LIKELY") {
-                setAnswer(key);
-            }
-        }
-        setAnswer("IMAGE NOT FOUND");
-    }
+//     // function rankEmotions() {
+//     //     for (var key in dict) {
+//     //         if (dict[key] === "VERY_LIKELY") {
+//     //             setAnswer(key);
+//     //         }
+//     //     } 
+//     //     // setAnswer("IMAGE NOT FOUND");
+//     // }
 
-    return (
-        <div>
-            <h1>
-                {answer}
-            </h1>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1>
+//                 {answer}
+//             </h1>
+//         </div>
+//     )
+// }
 
-export default Vision
+// export default Vision
