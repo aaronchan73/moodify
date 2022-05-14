66174ad7f99d90e7120620204bba28e69cd2d387
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-05-14 03:48:23 -0700

    vision dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import SongsList from './components/SongsList';
+import Vision from './components/Vision';
 
 
 function App() {
   return (
     <div className="App">
-      <SongsList />
+      <Vision />
     </div>
   );
 }
--- src/components/Vision.js
+++ src/components/Vision.js
@@ -1,57 +1,67 @@
-// import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+const vision = require('@google-cloud/vision');
+const { JWT } = require('google-auth-library');
 
-// const Vision = () => {
+const Vision = () => {
 
-//     const [dict, setDict] = useState({});
-//     const [answer, setAnswer] = useState("");
-//     const fileName = "./image.jpg"
+    const [dict, setDict] = useState({});
+    const [answer, setAnswer] = useState("");
+    const fileName = "./image.jpg"
+    const keys = require('./cloud-vision.json');
+    const client = new vision.ImageAnnotatorClient();
 
-//     const vision = require('@google-cloud/vision');
+    useEffect(() => {
+        authVision();
+        // rankEmotions();
+    }, []);
 
-//     const client = new vision.ImageAnnotatorClient();
+    const authVision = async () => {
+        const client = new JWT({
+            email: keys.client_email,
+            key: keys.private_key,
+            scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+        });
+        const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`;
+        const res = await client.request({ url });
+        console.log(res.data);
+    }
 
-//     useEffect(() => {
-//         detectFaces();
-//         console.log(dict)
-//         // rankEmotions();
-//     }, []);
+    const detectFaces = async () => {
 
-//     async function detectFaces() {
+        const result = await client.faceDetection(fileName);
+        const faces = await result.faceAnnotations;
+        console.log('Faces:');
+        faces.forEach((face, i) => {
+            console.log(`  Face #${i + 1}:`);
+            console.log(`    Joy: ${face.joyLikelihood}`);
+            console.log(`    Anger: ${face.angerLikelihood}`);
+            console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+            console.log(`    Surprise: ${face.surpriseLikelihood}`);
+            setDict({
+                joy: `${face.joyLikelihood}`,
+                anger: `${face.angerLikelihood}`,
+                sorrow: `${face.sorrowLikelihood}`,
+                surprise: `${face.surpriseLikelihood}`
+            });
+        });
+    }
 
-//         const result = await client.faceDetection(fileName);
-//         const faces = await result.faceAnnotations;
-//         console.log('Faces:');
-//         faces.forEach((face, i) => {
-//             console.log(`  Face #${i + 1}:`);
-//             console.log(`    Joy: ${face.joyLikelihood}`);
-//             console.log(`    Anger: ${face.angerLikelihood}`);
-//             console.log(`    Sorrow: ${face.sorrowLikelihood}`);
-//             console.log(`    Surprise: ${face.surpriseLikelihood}`);
-//             setDict({
-//                 joy: `${face.joyLikelihood}`,
-//                 anger: `${face.angerLikelihood}`,
-//                 sorrow: `${face.sorrowLikelihood}`,
-//                 surprise: `${face.surpriseLikelihood}`
-//             });
-//         });
-//     }
+    const rankEmotions = () => {
+        for (var key in dict) {
+            if (dict[key] === "VERY_LIKELY") {
+                setAnswer(key);
+            }
+        }
+        // setAnswer("IMAGE NOT FOUND");
+    }
 
-//     // function rankEmotions() {
-//     //     for (var key in dict) {
-//     //         if (dict[key] === "VERY_LIKELY") {
-//     //             setAnswer(key);
-//     //         }
-//     //     } 
-//     //     // setAnswer("IMAGE NOT FOUND");
-//     // }
+    return (
+        <div>
+            <h1>
+                {answer}
+            </h1>
+        </div>
+    )
+}
 
-//     return (
-//         <div>
-//             <h1>
-//                 {answer}
-//             </h1>
-//         </div>
-//     )
-// }
-
-// export default Vision
+export default Vision
